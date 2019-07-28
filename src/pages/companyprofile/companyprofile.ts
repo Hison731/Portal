6b2f0d72dd19c04300a29107689f6d930e35de0b
apple <whitegem_99@hotmailc.om>
2019-07-28 23:29:17 +0300

    2,3,5 added

--- src/pages/companyprofile/companyprofile.ts
+++ src/pages/companyprofile/companyprofile.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, LoadingController, ModalController} from 'ionic-angular';
 import { DataProvider } from '../../providers/data/data';
 // import { SampleModalPage } from '../sample-modal/sample-modal'; 
+import * as $ from 'jquery'
 @IonicPage()  
 @Component({
   selector: 'page-companyprofile',
@@ -9,11 +10,49 @@ import { DataProvider } from '../../providers/data/data';
 
 })
 export class CompanyprofilePage {
-  constructor(public modalCtrl: ModalController, public navCtrl: NavController) { }
+  constructor(public modalCtrl: ModalController, public navCtrl: NavController) { 
+  }
+  ionViewDidLoad() {
+    var showChar = 120;
+    var ellipsestext = "...";
+
+    $(".truncate").each(function() {
+      var content = $(this).html();
+      if (content.length > showChar) {
+        var c = content.substr(0, showChar);
+        var h = content;
+        var html =
+          '<div class="truncate-text" style="display:block">' +
+          c +
+          '<span class="moreellipses">' +
+          ellipsestext +
+          '&nbsp;&nbsp;<a href="" class="moreless more">Read More..</a></span></span></div><div class="truncate-text" style="display:none">' +
+          h +
+          '<a href="" class="moreless less">Read Less..</a></span></div>';
+
+        $(this).html(html);
+      }
+    });
+
+    $(".moreless").click(function() {
+      var thisEl = $(this);
+      var cT = thisEl.closest(".truncate-text");
+      var tX = ".truncate-text";
+
+      if (thisEl.hasClass("less")) {
+        cT.prev(tX).toggle();
+        // cT.slideToggle();
+        cT.toggle();
+      } else {
+        cT.toggle();
+        cT.next(tX).fadeToggle();
+      }
+      return false;
+    });
+  }
   // openModal() {
   //   let myModal = this.modalCtrl.create(SampleModalPage);
   //   myModal.present();
   // }
 }
-
 
